e1993aec9d0b9b02b3e3b6d0b7f4fafb6fe74bfd
DucHuy267 <155927918+DucHuy267@users.noreply.github.com>
2024-10-22 18:54:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('hello')
+import pygame
+
+import assets
+import configs
+from objects.background import Background
+from objects.bird import Bird
+from objects.column import Column
+from objects.floor import Floor
+from objects.gameover_message import GameOverMessage
+from objects.gamestart_message import GameStartMessage
+from objects.score import Score
+
+pygame.init()
+
+screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
+
+pygame.display.set_caption("Flappy Bird Game")
+
+img = pygame.image.load('assets/icons/red_bird.png')
+pygame.display.set_icon(img)
